41621a772fc13dbac07cf2aa33b839d9b6cd0c74
tbrown034 <tbrown034@gmail.com>
2023-12-06 21:14:40 -0500

    the blinking cursor works, but only if you have something in the div, but it covers it, maybe there is no width

---
+++ main.js
@@ -0,0 +1,21 @@
+//main.js
+
+//selectors
+
+const homeSection = document.getElementById("home-section");
+const playButton = document.getElementById("play-button");
+const aboutButton = document.getElementById("about-button");
+const computerSection = document.getElementById("computer-section");
+const gameSection = document.getElementById("game-section");
+
+// helper functions
+
+const handlePlayButton = () => {
+  homeSection.style.display = "none";
+  computerSection.style.display = "flex";
+};
+
+// event listeners
+
+playButton.addEventListener("click", handlePlayButton);
+aboutButton.addEventListener("click", handleAboutButton);
