26f2dad1c2063bfd7fa9732695bcb23d1d2ccecb
tbrown034 <tbrown034@gmail.com>
2023-12-07 20:04:01 -0500

    moved game-details object to seperate folder and now importing it into main.js

---
+++ data/game-details.js
@@ -0,0 +1,17 @@
+export const gameList = [
+  {
+    name: "Tic Tac Toe",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque.",
+  },
+  {
+    name: "Connect Four",
+    description:
+      "Dictumst vestibulum rhoncus est pellentesque. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Volutpat consequat mauris nunc congue. Convallis a cras semper auctor neque.",
+  },
+  {
+    name: "Rock Paper Scissors",
+    description:
+      "Quis auctor elit sed vulputate mi sit amet mauris commodo. Duis ut diam quam nulla. Nunc non blandit massa enim nec. Duis convallis convallis tellus id interdum velit laoreet id donec.",
+  },
+];
--- main.js
+++ main.js
@@ -3,25 +3,9 @@
 import { playRock } from "./scripts/rock.js";
 import { playConnect } from "./scripts/connect.js";
 import { playTic } from "./scripts/tic.js";
+import { gameList } from "./data/game-details.js";
 
 // Game data
-const gameList = [
-  {
-    name: "Tic Tac Toe",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque.",
-  },
-  {
-    name: "Connect Four",
-    description:
-      "Dictumst vestibulum rhoncus est pellentesque. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Volutpat consequat mauris nunc congue. Convallis a cras semper auctor neque.",
-  },
-  {
-    name: "Rock Paper Scissors",
-    description:
-      "Quis auctor elit sed vulputate mi sit amet mauris commodo. Duis ut diam quam nulla. Nunc non blandit massa enim nec. Duis convallis convallis tellus id interdum velit laoreet id donec.",
-  },
-];
 
 // DOM element selectors
 const homeSection = document.getElementById("home-section");
